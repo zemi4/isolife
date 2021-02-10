edb08f90e1cb163051cb4cdc4407a49a6c1e875f
zemi4 <zemi4foto@gmail.com>
2021-02-10 21:47:31 +0300

    menu fixed

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Когда пользователь прокручивает вниз 20px от верхней части документа, сдвиньте вниз навигационная панель
+// Когда пользователь прокручивает страницу до верха, сдвиньте ее вверх. navbar (50px из вида сверху)
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 620 || document.documentElement.scrollTop > 620) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+}
